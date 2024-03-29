628808d38df80222b3c58147f75d465932a72174
devjimy912 <devjimy912@gmail.com>
2024-03-29 16:33:36 +0900

    Fix struct library

--- HW4/C006.c
+++ HW4/C006.c
@@ -1,10 +1,9 @@
 #include <stdin.h>
 
 struct library{
-    char Name[50]; //book name
+    char name[50]; //book name
     int number; //book number
     int isCheckOut; //0:in, 1:out
-    int count; //how many
     char author[50]; //author
     char publisher[50]; //publisher
 };
