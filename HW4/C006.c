2bcb53f3717dc76fbc2b2878b8655c6492fc088d
devjimy912 <devjimy912@gmail.com>
2024-03-29 16:05:06 +0900

    Add struct

--- HW4/C006.c
+++ HW4/C006.c
@@ -1,10 +1,16 @@
 #include <stdin.h>
 
-/*
-ㅜ머ㄴ드지
-도서관으로 하자.
-*/
+struct library{
+    char Name[50]; //book name
+    int number; //book number
+    int isCheckOut; //0:in, 1:out
+    int count; //how many
+    char author[50]; //author
+    char publisher[50]; //publisher
+};
+
 
 int main(){
+
     return 0;
 }
