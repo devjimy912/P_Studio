3e6945307fc1053d3f5799c1a88d5576c43c5f79
devjimy912 <devjimy912@gmail.com>
2024-03-30 19:13:42 +0900

    Add addData

--- HW4/C006.c
+++ HW4/C006.c
@@ -76,7 +76,20 @@ int loadData(struct st_book* c[]){
 }
 
 int addData(int count, struct st_book* c[]){
-
+    struct st_book* new = (struct st_book*)malloc(sizeof(struct st_book));
+    printf("Book Name : ");
+    scanf("%s", new->name);
+    printf("Book Number : ");
+    scanf("%d", &(new->number));
+    printf("Author : ");
+    scanf("%s", new->author);
+    printf("Publisher : ");
+    scanf("%s", new->publisher);
+    new->isCheckOut = 0;
+    c[count] = new;
+    printf("Added new book\n");
+    printf("[%d] %s %d %s %s\n", count, c[count].name, c[count].number, c[count].author, c[count].publisher);
+    return count+1;
 }
 
 void readData(int count, struct st_book* c[]){
