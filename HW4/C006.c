908072240dd62aafcd20aa29ffe3a201159fae1f
devjimy912 <devjimy912@gmail.com>
2024-03-30 20:57:08 +0900

    Add editData

--- HW4/C006.c
+++ HW4/C006.c
@@ -111,7 +111,17 @@ void editData(int count, struct st_book* c[]){
     readData(c);
     printf("\nWhich book do you want to edit(input index) : ");
     scanf("%d", &index);
-    
+    printBookInfo(index-1, c);
+    printf("New Book Name : ");
+    scanf("%s", c[index]->name);
+    printf("New Book Number : ");
+    scanf("%d", &(c[index]->number));
+    printf("New Author : ");
+    scanf("%s", c[index]->author);
+    printf("New Publisher : ");
+    scanf("%s", c[index]->publisher);
+    printf("Edit Book No.%d\n", count);
+    printBookInfo(index-1, c);
 }
 
 int deleteData(int count, struct st_book* c[]){
