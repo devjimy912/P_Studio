f8797497e9ce635a53f08762843cd71d1841bfeb
devjimy912 <devjimy912@gmail.com>
2024-03-30 19:25:17 +0900

    Add readData

--- HW4/C006.c
+++ HW4/C006.c
@@ -93,7 +93,12 @@ int addData(int count, struct st_book* c[]){
 }
 
 void readData(int count, struct st_book* c[]){
-
+    int i;
+    char printCheck[2][50] = {"No loan possible", "On loan"};
+    printf("Book Data List\n");
+    for(i=0; i<count; i++){
+        printf("[%d] %s %d %s %s (%s)\n", count, c[count].name, c[count].number, c[count].author, c[count].publisher, printCheck[c[count].isCheckOut]);
+    }
 }
 
 void editData(int count, struct st_book* c[]){
