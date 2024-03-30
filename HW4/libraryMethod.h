7d4b9e8bf5fb34020f70dadb109bfff4898207ef
devjimy912 <devjimy912@gmail.com>
2024-03-30 22:50:00 +0900

    Add headerfile

---
+++ HW4/libraryMethod.h
@@ -0,0 +1,7 @@
+int addData(int count, struct st_book* c[]);
+void readData(int count, struct st_book* c[]);
+void editData(int count, struct st_book* c[]);
+int deleteData(int count, struct st_book* c[]);
+void searchData(int count, struct st_book* c[]);
+void saveData(int count, char filename[50], struct st_book* c[]);
+void printBookInfo(int num, struct st_book* c[]);
