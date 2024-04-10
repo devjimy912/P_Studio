3d71826668c09a9f704d438ccd99b7c3a88773a1
devjimy912 <devjimy912@gmail.com>
2024-04-10 13:07:10 +0900

    Add J037.Incomplete

--- week6/src/Main.java
+++ week6/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.*;
 
 class Main {
     public static void main(String[] args){
@@ -9,6 +10,8 @@ class Main {
         pstudio.J033();
         pstudio.J034();
         pstudio.J035();
+        pstudio.J036();
+        pstudio.J037(); //Incomplete
     }
 
     void J031(){
@@ -100,6 +103,37 @@ class Main {
         }
     }
     void J037(){//문자열 속 알파벳이 같은 수로 있는가
-
+        //1. 알파벳 별로 수를 저장한다.
+        //2. 문자열을 비교해 같은 알파벳이 나오면 인덱스에 무관하게 지우고 같은 과정을 반복하고 마지막에 들다 알파벳이 없으면 같은 것으로 한다.
+        //2번으로 하려면 추출하거나 문자열을 배열로 바꿔야하는데 반복의 특성상 배열이 변하니 문자 배열로 고치는 걸로 하고
+        //반복해서 비교 후 알파벳이 전부 없어지면 같은거
+        //하나라도 남거나 못 찾으면 다른거
+        Scanner sc = new Scanner(System.in);
+        String str1, str2;
+        str1 = sc.nextLine();
+        str2 = sc.nextLine();
+        boolean dummy = true;
+        char[] str1Array = str1.toCharArray();
+        char[] str2Array = str2.toCharArray();
+        for(int i=0; i<str1.length(); i++){
+            dummy = true;
+            if(Character.isAlphabetic(str1Array[i])) {
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str1Array[i] == str2Array[j]) {
+                        dummy = false;
+                        str2Array[j] = '$';
+                        break;
+                    }
+                }
+            }
+            if(dummy){
+                break;
+            }
+        }
+        if(dummy){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
     }
 }
