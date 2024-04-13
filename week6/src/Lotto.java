0845665ef832066ccba103366a5ade34daa89752
devjimy912 <devjimy912@gmail.com>
2024-04-13 14:47:04 +0900

    Add J041,J042,J043

--- week6/src/Lotto.java
+++ week6/src/Lotto.java
@@ -1,5 +1,8 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
+
+// J041
 public class Lotto {
     int[] numbers;
 
@@ -20,5 +23,35 @@ public class Lotto {
     }
     public void remakeAuto(){
         lotto();
+    }
+    //J042
+    public void make(){
+        Scanner sc = new Scanner(System.in);
+        for(int i=0; i<6; i++){
+            numbers[i] = sc.nextInt();
+            for(int j=0; j<i; j++){
+                if(numbers[i] == numbers[j]){
+                    i--;
+                    break;
+                }
+            }
+            if(numbers[i] < 1 || numbers[i] > 45){
+                System.out.println("Enter a number between 1 and 45");
+                i--;
+            }
+        }
+    }
+    //J043
+    public int checkLotto(int[] check){
+        int count=0;
+        for(int i=0; i<6; i++){
+            for(int j=0; j<i; j++){
+                if(numbers[i] == check[j]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
