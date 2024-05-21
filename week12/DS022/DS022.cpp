47f29440b3c463170fe681c8a70b8c4915b9d381
devjimy912 <devjimy912@gmail.com>
2024-05-22 02:05:03 +0900

    Add DS022

---
+++ week12/DS022/DS022.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+class ESG{
+    private:
+        string name;
+        int LC;
+        int RC;
+    public:
+        ESG();
+        ESG(string name,int lc, int rc);
+        string getName(){ return name; };
+        void setName(string n){ name=n; };
+        int getLC(){ return LC; };
+        void setLC(int lc){ LC = lc; };
+        int getRC(){ return RC; };
+        void setRC(int rc){ RC = rc; };
+        void printResult();
+        string check(int score);
+};
+
+int main(){
+    ESG esg;
+    string name;
+    int lc,rc;
+    //이거 세터 외부가 아니라 그냥 따로 메소드 만들어서 cin으로 받는 걸 만들까..
+
+    getline(cin, name);
+    esg.setName(name);
+
+    do{
+        cin >> lc;
+    }while(lc>=0 && lc<= 495);
+    esg.setLC(lc);
+
+    do{
+        cin >> rc;
+    }while(rc>=0 && rc<= 495);
+    esg.setRC(rc);
+
+    esg.printResult();
+
+    return 0;
+}
+
+ESG::ESG(){}
+
+ESG::ESG(string name, int lc, int rc){
+    this->name = name;
+    this->LC = lc;
+    this->RC = rc;
+}
+
+void ESG::printResult(){
+    cout << "["<<name<<"]"<<endl;
+    cout << "LC - " << LC << check(LC) << endl;
+    cout << "RC - " << RC << check(RC) << endl;
+    cout << "Total - " << LC+RC << endl;
+}
+
+string ESG::check(int score){
+    if(score < 350){
+        return "Fail";
+    }else{
+        return "Pass";
+    }
+}
