3e14d72a6fe13e26995c894198f3cc583b59f5ca
devjimy912 <devjimy912@gmail.com>
2024-06-01 16:50:51 +0900

    Add DS032

--- week13/DS032/main.cpp
+++ week13/DS032/main.cpp
@@ -1,1 +1,39 @@
-//Umm..
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Stack.h"
+
+using namespace std;
+
+int main() {
+    Stack<string> nameStack(100);
+    double maxGPA = 0.0;
+
+    ifstream file("data.txt");
+    if (!file.is_open()) {
+        cout << "No File." << endl;
+        return 1;
+    }
+
+    string name;
+    double gpa;
+    while (file >> gpa >> name) {
+        if (gpa > maxGPA) {
+            while (!nameStack.isEmpty()) {
+                nameStack.pop();
+            }
+            nameStack.push(name);
+            maxGPA = gpa;
+        } else if (gpa == maxGPA) {
+            nameStack.push(name);
+        }
+    }
+
+    cout << "Top GPA: " << maxGPA << endl;
+    while (!nameStack.isEmpty()) {
+        cout << nameStack.pop() << endl;
+    }
+
+    file.close();
+    return 0;
+}
