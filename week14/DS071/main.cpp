bdfe3b8ee5d990464cb14fc7561968f8d602874a
devjimy912 <devjimy912@gmail.com>
2024-06-08 13:47:14 +0900

    Update DS071

--- week14/DS071/main.cpp
+++ week14/DS071/main.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main(){
     //array size
     int size;
-    // cin>>size;
-    // int list[size];
+    cin>>size;
+    int list[size];
     //data
-    // for(int i=0; i<size; i++){
-    //     cin>>list[i];
-    // }
+    for(int i=0; i<size; i++){
+        cin>>list[i];
+    }
 
-    size = 5;
+    // size = 5;
 
-    int list[] = {64, 25, 12, 22, 11};
+    // int list[] = {64, 25, 12, 22, 11};
 
     print(list, size);
 
