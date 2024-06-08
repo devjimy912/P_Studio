52d9bc51b91fc5763aa659697477b671466eea31
devjimy912 <devjimy912@gmail.com>
2024-06-08 17:05:22 +0900

    Finish DS074

--- week14/DS074/QuickSort.h
+++ week14/DS074/QuickSort.h
@@ -1,1 +1,35 @@
+#include <iostream>
 
+void print(const int* arr, const int size){
+    for(int i=0; i<size; i++){
+        std::cout<<"["<<arr[i]<<"]";
+    }
+    std::cout<<std::endl;
+}
+
+void swap(int& a, int& b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void quickSort(int* list, int left, int right, int size) {
+    int pivot, i, j;
+    if (left < right) {
+        i = left; // left is the first index of the left sublist.     
+        j = right+1; // right is the last element index of the right sublist.
+        pivot = list[left];
+        do {
+            do i++; while(i <= right && list[i] < pivot);
+            do j--; while(j  > left  && list[j] > pivot);
+            if(i<j)
+                swap(list[i], list[j]);
+        } while (i<j);     
+        swap(list[left], list[j]);
+#ifdef DEBUGMODE
+    print(list, size);
+#endif
+        quickSort(list, left, j-1, size);
+        quickSort(list, j+1, right, size);
+    }
+}
---
+++ week14/DS074/main.cpp
@@ -0,0 +1,30 @@
+#include "QuickSort.h"
+
+using namespace std;
+
+int main(){
+    //array size
+    int size;
+    cin>>size;
+    int list[size];
+    //data
+    for(int i=0; i<size; i++){
+        cin>>list[i];
+    }
+
+    // size = 5;
+
+    // int list[] = {64, 25, 12, 22, 11};
+
+    print(list, size);
+
+    cout<<"sorting start"<<endl;
+
+    quickSort(list, 0, size-1, size);
+
+    cout<<"========="<<endl;
+
+    print(list, size);
+
+    return 0;
+}
