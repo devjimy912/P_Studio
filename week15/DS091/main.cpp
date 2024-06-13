b5baec06fb4e8ae5b824d0efc1caade111401f7c
devjimy912 <devjimy912@gmail.com>
2024-06-14 00:33:13 +0900

    Finish DS091

--- week15/DS091/main.cpp
+++ week15/DS091/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <string>
 
 using namespace std;
 
@@ -14,23 +15,49 @@ int main(){
     //char로 받아서 스택에 바로 넣는걸로
     //넣기 전에 <,> 확인해주고
 
-    char c;
-    bool q = true;
-    while(q){
+    string str; 
+    while(1){
         //일단 보류
         //q면 종료인데
         //지금은 문자열안에 q가 있으면 멈춰버림
         //그래서 문자열의 길이가 1이고 q인 경우로 바꿔야함
-        cin >> c;
-        if(c == 'q'){
-            q = false;
-        }else if(c == '<'){ //main -> sub
-
-        }else if(c == '>'){ //sub -> main
-
-        }else{
-
+        //str로 받고 for문으로 하자
+        stack<char> mainStack;
+        stack<char> subStack;
+        cin >> str;
+        if(str.length() == 1 && str == "q"){
+            break;
         }
+        
+        for(char c : str){
+            if(c == '<'){ //main -> sub
+                if(!mainStack.empty()){
+                    subStack.push(mainStack.top());
+                    mainStack.pop();
+                }
+            }else if(c == '>'){ //sub -> main
+                if(!subStack.empty()){
+                    mainStack.push(subStack.top());
+                    subStack.pop();
+                }
+            }else{
+                mainStack.push(c);
+            }
+        }
+        stack<char> print;
+        while(!subStack.empty()) {
+            mainStack.push(subStack.top());
+            subStack.pop();
+        }
+        while(!mainStack.empty()) {
+            print.push(mainStack.top());
+            mainStack.pop();
+        }
+        while(!print.empty()){
+            cout<<print.top();
+            print.pop();
+        }
+        cout<<endl;
     }
         
     return 0;
